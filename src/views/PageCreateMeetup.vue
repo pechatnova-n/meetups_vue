601d41b4cd5c4292b3bfc8efe26d7eaedac522d0
Natalia Pechatnova <pechatnova.n@yandex.ru>
2024-03-16 19:56:11 +0300

    добавлены компоненты для работы с календарем

--- src/views/PageCreateMeetup.vue
+++ src/views/PageCreateMeetup.vue
@@ -1,29 +1,56 @@
 <template>
-  <MeetupForm :meetup="meetup" />
+  <LayoutMeetupForm title="Создание митапа">
+    <MeetupForm :meetup="meetup" submitText="Создать" @submit="sendNewMeetup"  @cancel="cancelForm" />
+  </LayoutMeetupForm>
 </template>
 
 <script>
 import { ref } from 'vue';
 import MeetupForm from '../components/MeetupForm.vue';
 import { createMeetup } from '../services/meetupService.js';
+import UiPageTitle from "@/components/UiPageTitle.vue";
+import LayoutMeetupForm from "@/components/LayoutMeetupForm.vue";
+import {postMeetup} from "@/api/meetupsApi";
+import {router} from "@/router";
+import {klona} from "klona";
 
 export default {
   name: 'PageCreateMeetup',
 
   components: {
+    LayoutMeetupForm,
+    UiPageTitle,
     MeetupForm,
   },
 
   setup() {
-    // TODO: title "Создание митапа | Meetups"
-    // TODO: Добавить LayoutMeetupForm
+    // TODO: title "Создание митапа | Meetups"+
+    // TODO: Добавить LayoutMeetupForm+
     const meetup = ref(createMeetup());
 
-    // TODO: При сабмите формы создания митапа - добавить его через API и перейти на страницу созданного митапа
-    // TODO: При нажатии на "Отмена" вернуться на главную страницу
+
+
+    const sendNewMeetup = async (meetup) => {
+      await postMeetup(meetup)
+        .then((res) => {
+          console.log(res)
+          router.push({ name: "meetup", params: { meetupId: res.data.id} });
+        })
+        .catch(e => console.log(e.message));
+
+    }
+
+    const cancelForm = () => {
+      router.push({ name: 'index'});
+    }
+
+    // TODO: При сабмите формы создания митапа - добавить его через API и перейти на страницу созданного митапа+
+    // TODO: При нажатии на "Отмена" вернуться на главную страницу+
 
     return {
       meetup,
+      sendNewMeetup,
+      cancelForm,
     };
   },
 };
