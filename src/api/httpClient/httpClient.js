955b5e8646731aba31fd5cc3bee034a9dd13be01
Natalia Pechatnova <pechatnova.n@yandex.ru>
2024-05-15 08:14:43 +0300

    Revert "add headers to httpClient"
    This reverts commit 59b57a75ed6a0f9d1fca4233a988798e4458ce57.

--- src/api/httpClient/httpClient.js
+++ src/api/httpClient/httpClient.js
@@ -3,8 +3,7 @@ import { createErrorResult, createSuccessResult } from './ResultContainer.js';
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
-  withCredentials: true,
-  headers: {'Access-Control-Allow-Origin': '*'},
+  withCredentials: false,
   validateStatus(status) {
     /*
       401 Unauthenticated
